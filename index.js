e2e068d7824cf0861c720d757cbc72dadea1d7fb
Lucas Gomes <lucasagostini400@gmail.com>
2024-09-03 22:54:49 -0300

    Tipos de escopos no Javascript

---
+++ index.js
@@ -0,0 +1,30 @@
+// if(true){
+//     const mensagem = 'Olá'
+//     console.log(mensagem)
+// }
+
+// console.log(mensagem)
+
+// {
+//     var mensagem = 'Olá'
+//     console.log(mensagem)
+// }
+
+// console.log(mensagem)
+
+function falar(){
+    var mensagem = 'Olá Mundo!'
+    console.log(mensagem)
+    function falarOutraCoisa(){
+    
+    }
+
+    falarOutraCoisa()
+    console.log(falarOutraCoisa)
+}
+
+falar()
+console.log(mensagem)
+console.log(falarOutraCoisa)
+
+console.log(url)
